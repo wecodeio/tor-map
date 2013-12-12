ed240238654e5e1554f1fd142324e34109f365dd
Cristian Rasch <cristianrasch@gmail.com>
2013-12-12 11:18:41 -0300

    Force unzip the GeoLiteCity db

--- lib/ip_geocoder.rb
+++ lib/ip_geocoder.rb
@@ -5,12 +5,10 @@ class IPGeocoder
     attr_reader :db
 
     def bootstrap
-      tmp_dir = ENV["HOME"]
-      tmp_dir += "/tmp" unless File.writable?(tmp_dir)
-      dat_filename = "GeoLiteCity.dat"
-      dat_file = File.join(tmp_dir, dat_filename)
+      home_dir = ENV["HOME"]
+      dat_file = File.join(home_dir, "GeoLiteCity.dat")
       unless File.exists?(dat_file) && File.ctime(dat_file) > Time.now - 60*60*24
-        %x(wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz --directory-prefix=#{tmp_dir} && gunzip #{tmp_dir}/GeoLiteCity.dat.gz)
+        %x(wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz --directory-prefix=#{home_dir} && gunzip --force #{home_dir}/GeoLiteCity.dat.gz)
       end
 
       @db = GeoIP::City.new(dat_file, :filesystem)
